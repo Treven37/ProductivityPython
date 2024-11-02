20b2a141e78c68ebaae74d92f6962337afbe9e8c
MoriHearns <officialtreven37@gmail.com>
2024-11-02 22:10:39 +0800

    Changes

--- a.py
+++ a.py
@@ -7,20 +7,9 @@ if (localStorage.getItem('lvl')):
 	i = float(localStorage.getItem('lvl'))
 else:
 	i = 1
-if (int(i) > 1):
-	localStorage.setItem('exp', i)
-	a = input("Are you sure you want to open unproductive app? (Y/N)")
-	if (a == "Y"):
-		os.system("am start --user 0 -n com.beforesoft.launcher/com.beforesoftware.launcher.activities.LauncherActivity ")
-		os.system("history -c ")
-		os.system("clear ")
-		print("Level: ", "%.2f"%math.cbrt(i*i2*i3))
-else:
-	localStorage.setItem('exp', i)
-	a = input("\n\n\nAre you sure you want to open unproductive app? (Y/N)")
-	if (a == "Y"):
-		os.system("am start --user 0 -n com.beforesoft.launcher/com.beforesoftware.launcher.activities.LauncherActivity ")
-		os.system("history -c ")
-		os.system("clear ")
-		print("Level: ", localStorage.getItem('lvl'))
+
+os.system("history -c ")
+os.system("am start --user 0 -n com.beforesoft.launcher/com.beforesoftware.launcher.activities.LauncherActivity ")
+os.system("clear ")
+print("Level: ", localStorage.getItem('lvl'))
 	
--- time.py
+++ time.py
@@ -3,15 +3,17 @@ import time
 import os
 import math
 import random
+
 from localStoragePy import localStoragePy
 localStorage = localStoragePy('learn', 'text')
 if (localStorage.getItem('lvl')):
 	i = float(localStorage.getItem('lvl'))
 else:
 	i = 1
+	
 c = []
 d = []
-j = 0
+
 start = input("do you have 15mins? (y/n)")
 if start == "y":
 	print("task: \na. Improve Blood Flow\nb. Learn STEM or Connected Learnings\nc. Create")
@@ -25,20 +27,26 @@ if start == "y":
 		a = "Improve Blood Flow"
 	if a == "b":
 		a = "Learn STEM/Other | Connected Learnings"
+		os.system("am start --user 0 -n de.niklaskellner.floatingcounter/de.niklaskellner.floatingcounter.gui.activity.MainActivity ")
 	if a == "c":
 		a = "Create"
 	c.append(a)
 	d.append(taskEnd)
-	j=j+1
+	
 print(str(d[0]) + ": " + c[0])
 print("in progress")
-time.sleep(10*60)
-end = input("done?(y/h/n)")
-if end == "y":
-	i=i+1
-elif end == "h":
-	i=i+0.37
+meow = int(datetime.datetime.now().minute)
+while(int(d[0].minute) + 15 >= meow):
+	meow = int(datetime.datetime.now().minute)
+	time.sleep(1)
+while(int(d[0].minute) + 15 < meow): 
+	end = input("done?(y/h/n)")
+	if end == "y":
+		i=i+1
+	elif end == "h":
+		i=i+0.37
+	if (end == "y" or end == "n" or end == "h"):
+		break
 localStorage.setItem('lvl', float(i))
 os.system("clear ")
 print("Level: ", localStorage.getItem('lvl'))
-
